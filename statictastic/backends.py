18cf312192a5101e12fa4d042a683625edbf308f
Dan Loewenherz <dloewenherz@gmail.com>
2012-10-11 18:19:49 -0700

    use static url if it exists

--- statictastic/backends.py
+++ statictastic/backends.py
@@ -3,6 +3,7 @@ from django.core.files import storage as django_storage
 from storages.backends import s3boto
 
 STORAGE_BUCKET_NAME = getattr(settings, 'AWS_STATIC_STORAGE_BUCKET_NAME', getattr(settings, 'AWS_STORAGE_BUCKET_NAME', None))
+STATIC_URL = getattr(settings, 'STATIC_URL', None)
 
 class VersionedS3BotoStorage(s3boto.S3BotoStorage):
     def __init__(self, *args, **kwargs):
@@ -10,8 +11,11 @@ class VersionedS3BotoStorage(s3boto.S3BotoStorage):
         self.bucket_name = STORAGE_BUCKET_NAME
 
     def url(self, name):
-        url = super(VersionedS3BotoStorage, self).url(name)
-        return "{}?{}".format(url, settings.COMMIT_SHA[:5])
+        if STATIC_URL is None:
+            url = super(VersionedS3BotoStorage, self).url(name)
+            return "{}?{}".format(url, settings.COMMIT_SHA[:5])
+        else:
+            return "{}{}?{}".format(STATIC_URL, url, settings.COMMIT_SHA[:5])
 
 
 class VersionedFileSystemStorage(django_storage.FileSystemStorage):
