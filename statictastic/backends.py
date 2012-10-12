7947ae89659b6e58cbad526bb0c3b201360cad1c
Dan Loewenherz <dloewenherz@gmail.com>
2012-10-11 17:57:55 -0700

    add versioned file system storage
    also change the names of the classes

--- statictastic/backends.py
+++ statictastic/backends.py
@@ -1,14 +1,20 @@
 from django.conf import settings
+from django.core.files import storage as django_storage
 from storages.backends import s3boto
 
 STORAGE_BUCKET_NAME = getattr(settings, 'AWS_STATIC_STORAGE_BUCKET_NAME', getattr(settings, 'AWS_STORAGE_BUCKET_NAME', None))
 
-class PatchedS3BotoStorage(s3boto.S3BotoStorage):
+class VersionedS3BotoStorage(s3boto.S3BotoStorage):
     def __init__(self, *args, **kwargs):
-        super(PatchedS3BotoStorage, self).__init__(*args, **kwargs)
+        super(VersionedS3BotoStorage, self).__init__(*args, **kwargs)
         self.bucket_name = STORAGE_BUCKET_NAME
 
     def url(self, name):
-        url = super(PatchedS3BotoStorage, self).url(name)
+        url = super(VersionedS3BotoStorage, self).url(name)
         return "{}?{}".format(url, settings.COMMIT_SHA[:5])
 
+
+class VersionedFileSystemStorage(django_storage.FileSystemStorage):
+    def url(self, name):
+        url = super(VersionedFileSystemStorage, self).url(name)
+        return "{}?{}".format(url, settings.COMMIT_SHA[:5])
