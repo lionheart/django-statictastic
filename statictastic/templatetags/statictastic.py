76813c72c1adfe526477a6bb93bd48f45f8cfef6
Dan Loewenherz <dloewenherz@gmail.com>
2012-10-27 15:26:37 -0700

    add pipelining

--- statictastic/templatetags/statictastic.py
+++ statictastic/templatetags/statictastic.py
@@ -1,16 +1,17 @@
-from django import template
-from lxml import html
 import re
+from hashlib import md5
+
+from django.core.cache import cache
+from django import template
+from django.conf import settings
+from django.contrib.staticfiles import finders, storage
+from django.core.files.base import ContentFile
+from lxml import etree
+from lxml import html
+import cssmin
 import requests
 
 register = template.Library()
-
-# Pull out urls
-# Compile them
-# Download them
-# Combine them
-# Upload them
-# Return URL
 
 @register.tag
 def statictastic(parser, token):
@@ -25,21 +26,33 @@ class CompressNode(template.Node):
     def render(self, context):
         r = re.compile(r'[\n\r]')
         output = re.sub(r, '', self.nodelist.render(context)).strip()
-        root = html.fragments_fromstring(output)
-        elements_to_remove = []
-        css_urls = []
-        for element in root:
-            if element.tag == "link" and 'rel' in element.attrib and element.attrib['rel'] == "stylesheet":
-                elements_to_remove.append(element)
-                if settings.DEBUG:
-                    css_urls.append("{}{}".format(settings.BASE_URL, element.attrib['href']))
+        html_checksum = md5(output).hexdigest()
 
-        css = ""
-        for url in css_urls:
-            response = requests.get(url)
-            css += response.body
+        cache_key = "statictastic:{}".format(html_checksum)
+        pipelined_html = cache.get(cache_key)
+        if not pipelined_html:
+            input_elements = html.fragments_fromstring(output)
+            output_elements = []
+            compiled_css_content = ""
+            for element in input_elements:
+                if element.tag == "link" and 'rel' in element.attrib and element.attrib['rel'] == "stylesheet":
+                    element.make_links_absolute(settings.BASE_URL)
+                    response = requests.get(element.attrib['href'])
+                    compiled_css_content += response.content
+                elif element.tag == 'style' and 'type' in element.attrib and element.attrib['type'] == 'text/css':
+                    compiled_css_content += element.text_content()
+                else:
+                    output_elements.append(element)
 
-        print css
+            compiled_css_content = cssmin.cssmin(compiled_css_content)
+            staticstorage = storage.staticfiles_storage
+            checksum = md5(compiled_css_content).hexdigest()
+            filename = "bundled/css/{}.css".format(checksum)
+            staticstorage.save(filename, ContentFile(compiled_css_content))
+            output_elements.append(etree.Element("link", attrib={'rel': 'stylesheet', 'href': staticstorage.url(filename)}))
 
-        return output.upper()
+            pipelined_html = "".join(html.tostring(element) for element in output_elements)
+            cache.set(cache_key, pipelined_html, 86400)
+
+        return pipelined_html
 
