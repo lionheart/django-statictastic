4d85810b3584cb00d7356310e25d6beb82d00fff
Dan Loewenherz <dloewenherz@gmail.com>
2013-01-14 22:24:32 -0800

    remove cssmin from requirements

--- setup.py
+++ setup.py
@@ -21,6 +21,6 @@ setup(
         'statictastic.management.commands',
         'statictastic.templatetags',
     ],
-    install_requires=['requests', 'cssmin'],
+    install_requires=['requests'],
 )
 
