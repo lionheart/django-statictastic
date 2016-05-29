cb06865f83871df4001e68452336b8e6ad6cc7cb
Dan Loewenherz <dan@lionheartsw.com>
2016-05-29 13:32:12 -0500

    update setup script

--- setup.py
+++ setup.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env/python
 
 import os
-from statictastic import metadata
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
+metadata = {}
+execfile("bigstore/metadata.py", metadata)
+
 setup(
-    name = 'django-statictastic',
-    version = metadata.__version__,
-    license = metadata.__license__,
-    description = 'A fantastic way to sync your files to S3.',
-    author = metadata.__author__,
-    author_email = metadata.__email__,
+    name='django-statictastic',
+    version=metadata['__version__'],
+    license=metadata['__license__'],
+    description='A fantastic way to sync your files to S3.',
+    author=metadata['__author__'],
+    author_email=metadata['__email__'],
     packages=[
         'statictastic',
         'statictastic.management',
         'statictastic.management.commands',
         'statictastic.templatetags',
     ],
-    install_requires=['requests'],
+    install_requires=["requests>=2.0.0", "cssmin>=0.2.0", "lxml>=3.0.0"],
 )
 
