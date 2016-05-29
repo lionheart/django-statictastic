a0c37d54d73edd25652e5a7e3ec5cd9342bad15b
Dan Loewenherz <dan@lionheartsw.com>
2016-05-29 13:33:07 -0500

    bump to 1.0.0

--- statictastic/metadata.py
+++ statictastic/metadata.py
@@ -1,6 +1,6 @@
-__version__ = "0.7.0"
+__version__ = "1.0.0"
 __author__ = "Dan Loewenherz"
-__copyright__ = "Copyright 2012-2015, Dan Loewenherz"
+__copyright__ = "Copyright 2012-2016, Dan Loewenherz"
 __maintainer__ = "Dan Loewenherz"
 __email__ = "dan@lionheartsw.com"
 __license__ = "Apache 2.0"
