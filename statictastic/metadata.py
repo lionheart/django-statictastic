b7c88d3370c88ccaac13c0e0fe21bc3797514433
Dan Loewenherz <dan@lionheartsw.com>
2017-05-02 12:49:13 -0500

    update metadata

--- statictastic/metadata.py
+++ statictastic/metadata.py
@@ -1,6 +1,6 @@
-__version__ = "3.0.0"
+__version__ = "3.0.1"
 __author__ = "Dan Loewenherz"
-__copyright__ = "Copyright 2012-2016, Dan Loewenherz"
+__copyright__ = "Copyright 2012-2017, Dan Loewenherz"
 __maintainer__ = "Dan Loewenherz"
 __email__ = "dan@lionheartsw.com"
 __license__ = "Apache 2.0"
